d73a801cb0a104bfbf33cef71d28127e651630da
sp4rck <allie.matthieu@gmail.com>
2015-09-29 17:02:04 +0200

    Added friends factory with a form

--- TP4/js/app.js
+++ TP4/js/app.js
@@ -1,5 +1,7 @@
 var app = angular.module('myApp', ['ngRoute']);
 
+
+// création du routing
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/', {
@@ -13,13 +15,62 @@ app.config(function($routeProvider){
 	.otherwise({redirectTo : '/'})
 });
 
-app.controller('friendsController', function($scope){
-	$scope.friends = [{name:'John', phone:'555-1276'},
+// création d'une factory, ici l'exemple n'est pas très pertinent mais cela est considéré comme une bonne pratique
+app.factory('friendsFactory', function(){
+
+	var friends = [{name:'John', phone:'555-1276'},
 	{name:'Mary', phone:'800-BIG-MARY'},
 	{name:'Mike', phone:'555-4321'},
 	{name:'Adam', phone:'555-5678'},
 	{name:'Julie', phone:'555-8765'},
 	{name:'Juliette', phone:'555-5678'}];
+
+	var factory = {};
+
+	factory.getFriends = function(){
+		return friends;
+	};
+
+	factory.addFriend = function(name, phone){
+		friends.push({
+			name : name,
+			phone : phone
+		});
+	};
+
+	return factory;
+});
+
+app.controller('friendsController', function($scope, friendsFactory){
+	$scope.friends = [];
+
+
+	$scope.regexp = {
+		phone : /^0\d{9}$/i,
+		name : /^[a-zA-Z ]+$/,
+	};
+
+	$scope.friend = {
+		name : '',
+		phone : '',
+	};
+
+	function init(){
+		$scope.friends = friendsFactory.getFriends();
+	}
+
+	$scope.addFriend = function(isValid){
+		if(isValid){
+			friendsFactory.addFriend($scope.friend.name, $scope.friend.phone);
+			$scope.friend.name = '';
+			$scope.friend.phone = '';
+			// $scope.newFriendForm.phone.$dirty = false;
+			// $scope.newFriendForm.name.$dirty = false;
+			$scope.newFriendForm.$setPristine(true);
+		}
+	};
+
+	init();
 });
 
 app.controller('contactController',function($scope){
